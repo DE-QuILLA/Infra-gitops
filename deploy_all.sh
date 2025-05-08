f0893c525f0fe3edec48fad2129bef1e27fdee31
CoffeeIsNak <tjdqnzo@naver.com>
2025-05-09 01:18:52 +0900

    fix: deploy_all.sh fixed

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,8 +1,14 @@
-echo "I'am here\n"
-echo $(pwd)
+#!bin/bash
+set -e
 
-cd ./terraform && chmod +x deploy_infra.sh && ./deploy_infra.sh
+echo "\n전체 인프라 배포를 시작합니다.\n"
+
+echo "\n[terraform] 테라폼 적용, GCP 인프라 배포 시작\n"
+chmod +x ./terraform/deploy_infra.sh && ./terraform/deploy_infra.sh
+echo "\n[terraform] 테라폼 적용 완료, GCP 인프라 배포 완료\n"
 
 sleep 5
 
-cd ../deploy && ./deploy_stacks.sh
+echo "\n[deploy] 사용 스택 배포 시작\n"
+chmod +x ./deploy/deploy_stacks.sh && ./deploy/deploy_stacks.sh
+echo "\n[deploy] 사용 스택 배포 완료\n"
